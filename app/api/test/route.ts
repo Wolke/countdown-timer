480a92c42f77fe9411527c9b50bb712618057c90
wolke <wolkesau@gmail.com>
2025-06-18 08:16:31 +0800

    feat: Add dynamic and revalidation settings to OG image generation API

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,3 +1,6 @@
+export const dynamic = "force-static"
+export const revalidate = false
+
 export async function GET() {
   return new Response(JSON.stringify({ message: "Hello from OG API!" }), {
     headers: { "Content-Type": "application/json" },
